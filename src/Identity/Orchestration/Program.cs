f9a1f368f6bd8c1dce177e05b4dcedd474e8d9a5
John Josua Paderon <johnjosua.paderon@outlook.com>
2022-01-08 17:10:41 +0800

    Cofi Identity Orchestration

---
+++ src/Identity/Orchestration/Program.cs
@@ -0,0 +1,10 @@
+using Cofi.Identity.Orchestration;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
